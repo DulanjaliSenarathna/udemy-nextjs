8dc3b66626d85873e1834b0ae4f15c7615644707
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-09-19 20:25:29 +0530

    Preparing the App For State Management

--- Section 2/src/components/PostsList.jsx
+++ Section 2/src/components/PostsList.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Post from './Post'
 import classes from './PostsList.module.css'
+import NewPost from './NewPost'
 
 const PostsList = () => {
   return (
+    <>
+    <NewPost/>
     <ul className={classes.posts}>
          <Post author="Dulani" body="This is a body"/>
          <Post author="Mesi" body="This is a body2"/>
     </ul>
+    </>
+    
   )
 }
 
