a6697d8d4682e9e395433e8069632f7746d747e0
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-10-23 15:07:08 +0530

    Outputting List Data

--- Section 2/src/components/PostsList.jsx
+++ Section 2/src/components/PostsList.jsx
@@ -18,10 +18,12 @@ const addPostHandler = (postData) =>{
     <NewPost 
     onCancel={onStopPosting} onAddPost={addPostHandler}/>
     </Modal>)}
-    
-    <ul className={classes.posts}>
-         <Post author='Dula' body='This is good'/>
-    </ul>
+    {posts.length >0 && <ul className={classes.posts}>
+         {posts.map((post)=> <Post key={post.body} author={post.author} body={post.body}/>)}
+    </ul>}
+    {posts.length == 0 && <div style={{textAlign:'center', color:'white'}}>
+      <h2>No posts</h2>
+      </div>}
    
     </>
     
