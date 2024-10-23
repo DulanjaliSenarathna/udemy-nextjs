e49645ce3de084b85d2c6810095cd93c80e4b20d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-10-23 11:53:25 +0530

    Handling Form Submission

--- Section 2/src/components/PostsList.jsx
+++ Section 2/src/components/PostsList.jsx
@@ -7,26 +7,14 @@ import Modal from './Modal'
 const PostsList = ({isPosting, onStopPosting}) => {
 
   
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-
-   
-
-    const changeBodyHandler = (event) =>{
-       setEnteredBody(event.target.value);
-    } 
-    
-    const changeAuthorHandler = (event) =>{
-      setEnteredAuthor(event.target.value);
-   } 
   return (
     <>
     {isPosting && (<Modal onClose={onStopPosting}>
-    <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler}/>
+    <NewPost 
+    onCancel={onStopPosting}/>
     </Modal>)}
     
     <ul className={classes.posts}>
-         <Post author={enteredAuthor} body={enteredBody}/>
          <Post author='Dula' body='This is good'/>
     </ul>
    
