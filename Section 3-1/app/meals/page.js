7fac6b8eeafe0ddae53dcdd027ca2f11c047e7a0
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-11-01 20:13:45 +0530

    Outputting Meals Data & Images With Unknown Dimensions

--- Section 3-1/app/meals/page.js
+++ Section 3-1/app/meals/page.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import classes from './page.module.css'
+import Link from 'next/link'
+import MealsGrid from '@/components/meals/melas-grid'
 
 const Meals = () => {
   return (
-    <div>Meals</div>
+    <>
+
+<header className={classes.header}>
+    <h1>Delicious meals, created <span className={classes.highlight}>by you</span></h1>
+    <p>Choose your favourite recipe and cook it yourself.</p>
+    <p className={classes.cta}>
+      <Link href='/meals/share'>Share Your Favourite Recipe</Link>
+    </p>
+</header>
+<main className={classes.main}>
+  <MealsGrid meals={[]}/>
+</main>
+    </>
   )
 }
 
