a222e2b892446b902915badbae8d14c2461ee82f
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-11-01 08:25:29 +0530

    Using Client Components Efficiently

--- Section 3-1/components/main-header/main-header.js
+++ Section 3-1/components/main-header/main-header.js
@@ -1,11 +1,16 @@
+
 import React from 'react'
 import Link from 'next/link';
 import logoImg from '../../assets/logo.png';
 import styles from './main-header.module.css'
 import Image from 'next/image';
 import MainHeaderBackground from './main-header-background';
+import NavLink from './nav-link';
+
 
 const MainHeader = () => {
+
+  
     return (
         <>
             <MainHeaderBackground />
@@ -18,10 +23,10 @@ const MainHeader = () => {
                 <nav className={styles.nav}>
                     <ul>
                         <li>
-                            <Link href='/meals'>Browse Meals</Link>
+                           <NavLink href={'/meals'}>Browse Meals</NavLink>
                         </li>
                         <li>
-                            <Link href='/community'>Foodies Community</Link>
+                        <NavLink href={'/community'}>Foodies Community</NavLink>
                         </li>
                     </ul>
                 </nav>
