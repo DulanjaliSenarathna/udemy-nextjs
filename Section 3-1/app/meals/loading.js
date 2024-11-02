d1b32912774331dc33f33e91d082940e4f0b11eb
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-11-02 10:40:02 +0530

    Adding A Loading Page

---
+++ Section 3-1/app/meals/loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './loading.module.css';
+
+const MealsLoadingPage = () => {
+  return (
+    <p className={classes.loading}>Loading Data..</p>
+  )
+}
+
+export default MealsLoadingPage
