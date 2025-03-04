ad37d3cc1d02d6e0c564cdd0b4aa6daadf0d65fd
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2025-03-04 21:44:24 +0800

    Storing Uploaded Images In The Cloud (AWS S3)

--- Section 3-1/lib/meals.js
+++ Section 3-1/lib/meals.js
@@ -1,8 +1,11 @@
-import fs from 'node:fs';
-
+import { S3 } from '@aws-sdk/client-s3';
 import sql from 'better-sqlite3'
 import slugify from 'slugify';
 import xss from 'xss';
+
+const s3 = new S3({
+  region: 'us-east-1'
+});
 
 const db = sql('meals.db');
 
@@ -18,36 +21,45 @@ export function getMeal(slug){
     return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
 
-export async function saveMeal(meal){
-    meal.slug = slugify(meal.title, {lower: true});
-    meal.instructions = xss(meal.instructions);
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+ 
+  const extension = meal.image.name.split('.').pop();
+  const fileName = `${meal.slug}.${extension}`;
+ 
+  const bufferedImage = await meal.image.arrayBuffer();
 
-    const extension = meal.image.name.split('.').pop();
-    const fileName = `${meal.slug}.${extension}`;
-
-   const stream = fs.createWriteStream(`public/images/${fileName}`);
-
-    const bufferedImage = await meal.image.arrayBuffer();
-
-    stream.write(Buffer.from(bufferedImage),(error)=>{
-      if(error) {
-        throw new Error('Error in saving image!');
-      } 
+  try {
+    s3.putObject({
+      Bucket: 'nextjs-demo-users-image-dul',
+      Key: fileName,
+      Body: Buffer.from(bufferedImage),
+      ContentType: meal.image.type,
     });
 
-    meal.image = `/images/${fileName}`
+    console.log('✅ Image uploaded successfully:', fileName);
 
-    db.prepare(`
-        INSERT INTO meals
-        (title, summary, instructions, creator, creator_email, image, slug)
-        VALUES (
-        @title,
-        @summary,
-        @instructions,
-         @creator,
-         @creator_email,
-        @image,
-         @slug
-         )
-        `).run(meal)
+  } catch (error) {
+    console.error('S3 Upload Error:', error);
+  }
+ 
+ 
+  meal.image = fileName;
+ 
+  db.prepare(
+    `
+    INSERT INTO meals
+      (title, summary, instructions, creator, creator_email, image, slug)
+    VALUES (
+      @title,
+      @summary,
+      @instructions,
+      @creator,
+      @creator_email,
+      @image,
+      @slug
+    )
+  `
+  ).run(meal);
 }
--- Section 3-1/next.config.js
+++ Section 3-1/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nextjs-demo-users-image-dul.s3.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
