34b47e3d35c5051900babfc09bcd4eeb93712ba8
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2025-03-24 14:16:19 +0800

    Combining Parallel & Intercepting Routes

--- Section 4/app/news/[slug]/@modal/(.)image/page.js
+++ Section 4/app/news/[slug]/@modal/(.)image/page.js
@@ -12,8 +12,12 @@ export default function InterceptedImagePage({params}){
         }
     return (
         <>
-        <h2>Intercepted!</h2>
+        <div className="modal-backdrop">
+            <dialog className="modal" open>
     <div className="fullscreen-image">
         <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/>
-    </div></>)
+    </div>
+    </dialog>
+    </div>
+    </>)
 }
