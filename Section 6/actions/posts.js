49907c15476e21d95e0c501ab2d151b7a72ded6b
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2025-03-30 17:32:23 +0800

    Uploading & Storing Images

--- Section 6/actions/posts.js
+++ Section 6/actions/posts.js
@@ -1,4 +1,5 @@
 'use server';
+import { uploadImage } from '@/lib/cloudinary';
 import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
 
@@ -25,8 +26,16 @@ export async function createPost(prevState,formData) {
       return {errors};
     }
 
+    let imageUrl;
+
+    try {
+         imageUrl = await uploadImage(image);
+    } catch (error) {
+        throw new Error('Image upload failed, post was not created!')
+    }
+
    await storePost({
-      imageUrl: '',
+      imageUrl: imageUrl,
       title,
       content,
       userId: 1
